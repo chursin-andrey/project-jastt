24ad0c28840376fd6a1d6ead3f3f65974599e9ca
buslov83@gmail.com <buslov83@gmail.com@c63669ad-59c2-068f-2305-3421648fb908>
2014-11-10 05:49:20 +0000

    Edit jira service interfaces

---
+++ trunk/src/main/java/com/jastt/business/services/jira/impl/JIRAConnectionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jastt.business.services.jira.impl;
+
+class JIRAConnectionServiceImpl implements JIRAConnectionService {
+
+	@Override
+	public JIRAConnection getJIRAConnection(String serverURL, String user,
+			String password) {
+		
+		return null;
+	}
+
+}
