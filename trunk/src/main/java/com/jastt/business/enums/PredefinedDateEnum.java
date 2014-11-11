e6a10a92c044b7f6c96d0cfdf98c08eca2562963
bogdanov005@gmail.com <bogdanov005@gmail.com@c63669ad-59c2-068f-2305-3421648fb908>
2014-11-11 12:40:12 +0000

    added PredefinedDateEnum

---
+++ trunk/src/main/java/com/jastt/business/enums/PredefinedDateEnum.java
@@ -0,0 +1,6 @@
+package com.jastt.business.enums;
+
+public enum PredefinedDateEnum {
+	ALL_TIME, TODAY, YESTERDAY, THIS_WEEK, LAST_WEEK, LAST_SEVEN_DAYS, 
+	THIS_MONTH, LAST_MONTH, LAST_THIRTY_DAYS, THIS_YEAR
+}
