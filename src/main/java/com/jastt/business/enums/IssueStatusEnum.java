9c7b477176d0fcfee65104e2543beceb90adeedc
bogdanov005 <bogdanov005@gmail.com>
2014-11-11 12:14:42 +0000

    Added enums and UserManagementService interface.

---
+++ src/main/java/com/jastt/business/enums/IssueStatusEnum.java
@@ -0,0 +1,5 @@
+package com.jastt.business.enums;
+
+public enum IssueStatusEnum {
+	OPEN, IN_PROGRESS, RESOLVED, REOPENED, CLOSED
+}
---
+++ src/main/java/com/jastt/business/enums/IssueTypeEnum.java
@@ -0,0 +1,5 @@
+package com.jastt.business.enums;
+
+public enum IssueTypeEnum {
+	BUG, IMPROVEMENT, NEW_FEATURE, TASK
+}
