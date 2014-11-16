bf2242ee5f9143bb3be94a416d28558d5b3b4c84
arturkozyrev@gmail.com <arturkozyrev@gmail.com@c63669ad-59c2-068f-2305-3421648fb908>
2014-11-16 10:44:22 +0000

    added temp methods for service layer

--- trunk/src/main/java/com/jastt/dal/providers/UserDataProvider.java
+++ trunk/src/main/java/com/jastt/dal/providers/UserDataProvider.java
@@ -1,5 +1,7 @@
 package com.jastt.dal.providers;
 
+import java.util.List;
+
 import com.jastt.dal.entities.UserEntity;
 import com.jastt.dal.providers.base.PageableDataProvider;
 import com.jastt.business.domain.entities.User;
@@ -12,6 +14,9 @@ public interface UserDataProvider extends PageableDataProvider<UserEntity, User,
 	public void updatePassword(User user, String newPassword);
 	public User getUserByLogin(String login);
 	public User getUserByEmail(String email);
+	public List<User> getAllUsers();
+	public void addUser(User user);
+	public void editUser(User user);
 	
 	
 	
