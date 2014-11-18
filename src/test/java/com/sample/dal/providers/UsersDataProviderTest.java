389ae63e181df89c1422975226bcc628ce480d7e
arturkozyrev <arturkozyrev@gmail.com>
2014-11-18 23:14:05 +0000

    Created UsersDataProviderTest.java for testing select/insert queries and changed configs for this purpose

--- src/test/java/com/sample/dal/providers/UsersDataProviderTest.java
+++ src/test/java/com/sample/dal/providers/UsersDataProviderTest.java
@@ -11,6 +11,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -26,7 +27,6 @@ import com.sample.utils.TestConstants;
 @ContextConfiguration(locations = { "classpath:testApplicationContext.xml" })
 public class UsersDataProviderTest {
 
-	@Autowired
 	private UsersDataProvider dataProvider;
 
 	@BeforeClass
