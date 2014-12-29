983733ac71a48242ada48070d0b08fddd6a25f4c
chursin <chursin@cs.vsu.ru>
2014-12-29 00:08:39 +0000

    Added custom EmailValidator and annotations (RequestScope, ViewScope). Added ability to add users with diff roles (but without editing).

---
+++ src/main/java/com/jastt/utils/annotations/ViewScope.java
@@ -0,0 +1,14 @@
+package com.jastt.utils.annotations;
+
+import org.springframework.context.annotation.Scope;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Scope("view")
+public @interface ViewScope {
+}
