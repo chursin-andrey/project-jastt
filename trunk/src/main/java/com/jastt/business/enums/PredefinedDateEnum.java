420cea8e48345edc1fd855e6445226fef3079021
buslov83@gmail.com <buslov83@gmail.com@c63669ad-59c2-068f-2305-3421648fb908>
2015-01-18 14:34:15 +0000

    Add time filtering to worklog page.

--- trunk/src/main/java/com/jastt/business/enums/PredefinedDateEnum.java
+++ trunk/src/main/java/com/jastt/business/enums/PredefinedDateEnum.java
@@ -1,7 +1,13 @@
 package com.jastt.business.enums;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 public enum PredefinedDateEnum {
 	
@@ -40,6 +46,72 @@ public enum PredefinedDateEnum {
 	
 	public String getDescription(){
 		return description;
+	}
+	
+	public Pair<Date, Date> getPeriod() {
+		MutablePair<Date, Date> pairOfDates = new MutablePair<Date, Date>();
+		
+		Calendar calendar = Calendar.getInstance();
+		Calendar baseDate = DateUtils.truncate(calendar, Calendar.DATE);
+		Calendar baseMonth = DateUtils.truncate(calendar, Calendar.MONTH);
+		Calendar baseYear = DateUtils.truncate(calendar, Calendar.YEAR);
+		pairOfDates.setRight(calendar.getTime());
+		
+		switch (this) {
+			case ALL_TIME:{
+				pairOfDates.setRight(null);
+				break;
+			}
+			case TODAY:{
+				pairOfDates.setLeft(baseDate.getTime());
+				break;
+			}
+			case YESTERDAY:{
+				pairOfDates.setRight(baseDate.getTime());
+				baseDate.add(Calendar.DATE, -1);
+				pairOfDates.setLeft(baseDate.getTime());
+				break;
+			}
+			case THIS_WEEK:{
+				baseDate.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+				pairOfDates.setLeft(baseDate.getTime());
+				break;
+			}
+			case LAST_WEEK:{
+				baseDate.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+				pairOfDates.setRight(baseDate.getTime());
+				baseDate.add(Calendar.DATE, -7);
+				pairOfDates.setLeft(baseDate.getTime());
+				break;
+			}
+			case LAST_SEVEN_DAYS:{
+				baseDate.add(Calendar.DATE, -7);
+				pairOfDates.setLeft(baseDate.getTime());
+				break;
+			}
+			case THIS_MONTH:{
+				pairOfDates.setLeft(baseMonth.getTime());
+				break;
+			}
+			case LAST_MONTH:{
+				pairOfDates.setRight(baseMonth.getTime());
+				baseMonth.add(Calendar.MONTH, -1);
+				pairOfDates.setLeft(baseMonth.getTime());
+				break;
+			}
+			case LAST_THIRTY_DAYS:{
+				baseDate.add(Calendar.DATE, -30);
+				pairOfDates.setLeft(baseDate.getTime());
+				break;
+			}
+			case THIS_YEAR:{
+				pairOfDates.setLeft(baseYear.getTime());
+				break;
+			}
+			default: break;
+		}
+		
+		return pairOfDates;
 	}
 	
 	private PredefinedDateEnum(String description) {
