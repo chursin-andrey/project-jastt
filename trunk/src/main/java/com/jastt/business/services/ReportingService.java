2ef536303f49846e40f3f598c450dc9d2f28f93a
buslov83@gmail.com <buslov83@gmail.com@c63669ad-59c2-068f-2305-3421648fb908>
2015-01-23 09:10:51 +0000

    Update report generation for Issue page. Change jasper report templates.

--- trunk/src/main/java/com/jastt/business/services/ReportingService.java
+++ trunk/src/main/java/com/jastt/business/services/ReportingService.java
@@ -2,14 +2,19 @@ package com.jastt.business.services;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import net.sf.jasperreports.engine.JRException;
 
 import com.jastt.business.domain.entities.Issue;
 
 public interface ReportingService {
-	public void exportToPdf(List<Issue> issues, OutputStream outStream) throws JRException, IOException;
-	public void exportToXlsx(List<Issue> issues, OutputStream outStream) throws JRException, IOException;
-	public void exportToXls(List<Issue> issues, OutputStream outStream) throws JRException, IOException;
+	public void exportToPdf(String reportFileName, OutputStream outStream, Map<String, Object> params, 
+			Collection<?> beanCollection) throws JRException, IOException;
+	public void exportToXlsx(String reportFileName, OutputStream outStream, Map<String, Object> reportParams, 
+			Collection<?> beanCollection) throws JRException, IOException;
+	public void exportToXls(String reportFileName, OutputStream outStream, Map<String, Object> reportParams, 
+			Collection<?> beanCollection) throws JRException, IOException;
 }
